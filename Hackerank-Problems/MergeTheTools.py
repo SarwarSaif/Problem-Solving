9f0bc949bb7814f7e253b69dc2e03851b63df9e8
Sarwar Saif <37659346+SarwarSaif@users.noreply.github.com>
2021-10-07 16:47:14 +0900

    Create MergeTheTools.py

---
+++ Hackerank-Problems/MergeTheTools.py
@@ -0,0 +1,25 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    # get the length of the string
+    n = len(string)
+    # If length is 1, print the string
+    if n==1:
+        print(string)
+    else:
+        # Divide the string into n/k substrings
+        # Dividend 0
+        substring_len = n//k
+        for i in range(0, n, substring_len):
+            substring = string[i:i+substring_len]
+            # As order of the subsequence matters, we can use dict to remove duplicate characters
+            result = "".join(dict.fromkeys(substring))
+            print(result)
+        if (n%k!=0):
+            #print the excess part
+            left_substring = string[(n-(n%k)):n]
+            result = "".join(dict.fromkeys(left_substring))
+            print(result)    
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)

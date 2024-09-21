16a474b67f96742699cf9d9b763f1d9113af6f4d
SARWAR SAIF <sarwarsaif@SARWARs-MacBook-Air.local>
2024-09-22 02:30:38 +0900

    added pattern solving

---
+++ Patterns/create_pattern.c
@@ -0,0 +1,65 @@
+# include <stdio.h>
+
+int main() {
+    int n,sum = 0;
+    printf("Enter Dimension of nxn matrix: ");
+    scanf("%d", &n);
+    int mid = ((n-1)/2)+1;
+    int a[n+1][n+1];
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            // scanf("%d", &a[i][j]);
+            a[i][j] = 0;
+        }
+    }
+    printf("\n");
+
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            // First pattern Mid columns + 1 && Not mid row and first row and last row
+            if (i==1 || i== mid || i==n){
+                if (i==1 && j%2!=0){
+                    sum += a[i][j];
+                    printf("%d\t", a[i][j]);
+                }
+                else if (i==mid && j%2==0){
+                    sum += a[i][j];
+                    printf("%d\t", a[i][j]);
+                }
+                else if (i==n && j>1 && j<n){
+                    sum += a[i][j];
+                    printf("%d\t", a[i][j]);
+                }
+                else {
+                    printf("C\t");
+                }
+                
+            }
+            else{
+                if ((j==mid-1) || (j==mid+1)){
+                    sum += a[i][j];
+                    // printf("i = %d j= %d : %d\n", i, j, a[i][j]);
+                    printf("%d\t", a[i][j]);
+
+                }
+                else{
+                    printf("C\t");
+                }
+                
+            }
+        }
+        printf("\n");
+    }
+    
+    
+
+    return 0;
+}
